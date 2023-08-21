293b10817b459f4465e7e1eb1148c1d24447263a
AhmedEnnaime <97989224+AhmedEnnaime@users.noreply.github.com>
2023-08-21 13:40:30 +0100

    feat: :bricks: still working to fix connection between client and server

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -3,31 +3,34 @@ package main
 import (
 	"context"
 	"log"
-	"os"
+	"time"
 
 	"github.com/AhmedEnnaime/SnapEvent/pb"
 	"github.com/AhmedEnnaime/SnapEvent/utils"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials/insecure"
-	"google.golang.org/grpc/status"
 )
 
 func main() {
 
-	clientAddr := os.Getenv("CLIENT_ORIGIN")
-	if clientAddr == "" {
-		log.Fatal("$CLIENT_ORIGIN is not set")
-	}
+	// clientAddr := os.Getenv("CLIENT_ORIGIN")
+	// if clientAddr == "" {
+	// 	log.Fatal("$CLIENT_ORIGIN is not set")
+	// }
 
-	log.Printf("client address: %s", clientAddr)
+	// log.Printf("client address: %s", clientAddr)
 
-	conn, err := grpc.Dial(clientAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err := grpc.Dial("server:8080", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatal("cannot dial server: ", err)
 	}
 
+	defer conn.Close()
+	log.Println(" connection state ====> ", conn.GetState())
 	userClient := pb.NewUserServiceClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+	defer cancel()
 
 	user := utils.NewUser()
 	req := &pb.CreateUserRequest{
@@ -38,15 +41,10 @@ func main() {
 		Gender:   user.Gender,
 	}
 
-	res, err := userClient.CreateUser(context.Background(), req)
+	res, err := userClient.CreateUser(ctx, req)
+
 	if err != nil {
-		st, ok := status.FromError(err)
-		if ok && st.Code() == codes.AlreadyExists {
-			log.Print("user already exists")
-		} else {
-			log.Fatal("cannot create user ", err)
-		}
-		return
+		log.Fatalf("Create user failed: %v", err)
 	}
 	log.Printf("user created %s", res)
 
